c10f3d055163c32341805f7ab9bfd756db3234d2
Zhao Ru Wei <934566509@qq.com>
2023-02-13 15:34:55 +0800

    Valentine's Day

---
+++ src/main/java/com/example/withlove/WithloveApplication.java
@@ -0,0 +1,13 @@
+package com.example.withlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithloveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithloveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/withlove/WithloveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.withlove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithloveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
